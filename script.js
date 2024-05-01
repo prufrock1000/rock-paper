7775781d1fbe1c64e87d811c7d776a25c29381de
Prufrock1000 <165000959+prufrock1000@users.noreply.github.com>
2024-05-01 20:28:40 +0100

    add first function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Hello World!')
+
+const items =  ['Rock','Paper','Scissors']
+
+function getComputerChoice(items) {
+    const random = Math.floor(Math.random()*items.length);
+  return items[random];
+    
+}
+console.log(getComputerChoice(items))
